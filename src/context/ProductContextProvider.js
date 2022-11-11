4bf858b3ab48273c04b638107fcf1262215edfc3
ZuhraMuk <zmu3842@gmail.com>
2022-11-11 12:03:16 +0300

    dep

--- src/context/ProductContextProvider.js
+++ src/context/ProductContextProvider.js
@@ -4,7 +4,7 @@ import { useLocation, useNavigate } from "react-router-dom";
 
 export const productContext = createContext();
 
-const API = "http://localhost:8000/products";
+const API = "https://online-shop-za.herokuapp.com/products";
 
 const INIT_STATE = {
   products: null,
